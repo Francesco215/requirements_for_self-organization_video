f029d21ce858d8b6a1a67d52c6a706f391b7cb47
Francesco215 <f.sacco@protonmail.com>
2023-11-25 15:08:57 +0100

    written address-value animation for hopfield

--- project/hopfield.py
+++ project/hopfield.py
@@ -32,3 +32,19 @@ class Hop(Scene):
         self.add(ax, labels, graph, dot)
         self.play(t.animate.set_value(x_space[minimum_index]))
         self.wait()
+
+from numpy.random import randint
+
+
+def random_binary_string():
+    return f'{randint(2**6):6b}'.replace(' ','0')
+
+class AddressTable(Scene):
+    def construct(self):
+        addresses=['0x'+random_binary_string() for _ in range(10)]
+        values   =[     random_binary_string() for _ in range(10)]
+        table=np.transpose(np.array([addresses,values]))
+
+        table_tex=Table(table, col_labels=[Text('Address'),Text('Value')]).scale(0.5)
+
+        self.play(Write(table_tex))
